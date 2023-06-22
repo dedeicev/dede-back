02172b7d0df1508b891cfb80c075522c6ec3cccf
ailsonazevedo <ailsonsilvaazevedo1@gmail.com>
2023-06-21 23:31:26 -0300

    bugfix: fix update and delete object user

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user'
+import addressRoutes from './routes/address'
+import customerRoutes from './routes/customer'
 
 dotenv.config()
 
@@ -9,6 +11,8 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use('/users', userRoutes)
+app.use('/addresses', addressRoutes)
+app.use('/customers', customerRoutes)
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
